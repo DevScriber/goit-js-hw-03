03b7f4f3d638ffba25c421a2a9b8083b0594f36b
Eugene Mukhin <eug.muhin@gmail.com>
2025-01-08 11:56:06 +0200

    add functions

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,20 @@
-function makeTransaction(quantity, pricePerDroid) {
-  const totalPrice = quantity * pricePerDroid;
-  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+// Создание генератора slug
+
+// Создаем функцию которая принимает 1 параметр - строка
+
+function slugify(title) {
+    // так как title это строка, нам нужно преобразовать в массив
+    const titleArray = title.split(' ')
+    // объединим элементы массива в строку и преобразуем строку в нижний регистр
+    const slug = titleArray.join('-').toLowerCase()
+    //
+    return slug
 }
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+
+
+console . log (slugify( "Arrays for beginners" )); // "arrays-for-beginners" 
+console . log (slugify( "English for developer" )); // "english-for-developer" 
+console . log (slugify( "Ten secrets of JavaScript" )); // "ten-secrets-of-javascript" 
+console . log (slugify( "How to become a JUNIOR developer in TWO WEEKS" )); // "how-to-become-a-junior-developer-in-two-weeks"
